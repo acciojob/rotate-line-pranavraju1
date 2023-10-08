17ac9262cf2f331ca6d2aa2583ddf3ce321077d3
Pranav Raju <120582474+pranavraju1@users.noreply.github.com>
2023-10-08 15:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    var line = document.getElementById("line");
+    var angle = 0;
+    function rotateLine() {
+      angle += 2; 
+      line.style.transform = "rotate(" + angle + "deg)";
+    }
+    setInterval(rotateLine, 20);
+  });
